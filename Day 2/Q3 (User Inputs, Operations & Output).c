ab94cf0e6d739f5bdc0a5d05c17b89c9fa2c5e4c
Sudhanshu-Jha312 <sudhanshujha9997@gmail.com>
2026-08-14 18:00:00 +0530

    Add Day 2 solutions

---
+++ Day 2/Q3 (User Inputs, Operations & Output).c
@@ -0,0 +1,13 @@
+/* Write a program to calculate the area and perimeter of a rectangle given its length and breadth. */
+#include <stdio.h>
+
+int main(void)
+{
+    double length, breadth;
+
+    if (scanf("%lf %lf", &length, &breadth) != 2 || length < 0.0 || breadth < 0.0) {
+        return 1;
+    }
+    printf("Area=%.15g, Perimeter=%.15g\n", length * breadth, 2.0 * (length + breadth));
+    return 0;
+}
